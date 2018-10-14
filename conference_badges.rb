499084026f58e4f831041108ddb24814ebf130a6
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-14 02:48:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,8 +5,9 @@ def badge_maker(name)
 end
 
 batch_badge_creator(names)
-  formatted_names
+  formatted_names = []
   names.each do |i|
     formatted_names.push("Hello, my name is #{i}.")
   end
+  return formatted_names
 end
